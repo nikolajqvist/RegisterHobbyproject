f5d25f4ad247da841d19b0e7884e1b56a90162e0
Nikolaj Qvist <nqvistmoller@hotmail.com>
2025-04-02 16:46:17 +0200

    kan get og post

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 const app = Vue.createApp({
     data () {
         return{
-            intro: 'Velkommen til min Vue Template'
+            intro: 'Velkommen til min Vue Template',
+            animals: [],
+            category: '',
+            age: '',
+            price: ''
         }
     },
+    mounted () {
+        axios
+          .get('http://localhost:5200/api/Animal')
+          .then(response => (this.animals = response.data))
+    },
     methods: {
+        postanimal() {
 
+            axios.post('http://localhost:5200/api/Animal', {
+                category: this.category,
+                age: this.age,
+                price: this.price
+            }).then(response => {
+                this.animals.push(response.data)
+                this.category = '',
+                this.age = '',
+                this.price = ''
+            })
+        }
     },
     computed: {
 
